a9b87ff6481cc21f1d661ca386c48cd73d1a7c49
Bardisk <litrehinn@gmail.com>
2024-01-23 11:55:50 +0800

    [FUNCTION] add kmeans wrapper

---
+++ birthmark_generation/kmeans/kmeans.py
@@ -0,0 +1,13 @@
+import json
+import os
+import subprocess
+
+with open('config.json', 'r') as file:
+    config = json.load(file)
+    n_clusters = config['n_clusters']
+    n_iters = config['n_iters']
+    dataset = config['dataset']
+    kmeans_output = config['kmeans_output']
+
+os.system('make kmeans')
+subprocess.run(f'sh -c \"./kmeans -c {n_clusters} -t {n_iters} < {dataset} > {kmeans_output}\"', shell=True)
